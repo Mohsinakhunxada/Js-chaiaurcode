9b1a35b1639177871ebd602f059cba774895e556
Mohsinakhunxada <mohsinakx7773@gmail.com>
2025-06-11 07:50:36 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+// Different data Types
+
+// number -> 2 to power 53
+// bigint -> greaer than number, used in stock market and huge websites like reddit
+// srting -> should be enclosed in " ", can contain any alphabet and number
+// boolean -> has yes/no or true/false possibilities
+// null -> when data is zero, that not present or cannot be fetched, it is assigned and defined value
+// undefined -> data set is given but value is not defined
+// symbol -> it is added for uniqueness of data
+
+
+// object -> will be studied in further tutorials
+
+// null is an object but undefined is a type in itself
+// it is an interview question
+
+
+age = 18
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof 18);
+console.log(typeof age);
+
+
+
+
+
