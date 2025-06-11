1d8192bdec048cf026119bf2d2a6df6694c8e209
Mohsinakhunxada <mohsinakx7773@gmail.com>
2025-06-11 08:18:35 +0000

    Conversions

---
+++ 01_Basics/03_Conversions.js
@@ -0,0 +1,21 @@
+let score = "14sa"
+
+let StringAbility = Number (score)
+
+console.log(typeof StringAbility);
+console.log(StringAbility);
+
+// JS will convert everything in the "type" you command of, but if you want to see its reality; console log will show
+// when you apply "" to number, it becomes string
+// "14" -> into number 14
+// "14abc" -> NaN (Not a Number) but its type will be number
+// True -> 1; false -> 0
+
+let loggedIn = 0
+
+let booleanloggedIn = Boolean(loggedIn)
+console.log(booleanloggedIn);
+console.log(typeof booleanloggedIn);
+
+
+
