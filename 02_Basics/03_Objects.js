0088d97fe5619a6908a59e811c13b2473714d443
Mohsinakhunxada <mohsinakx7773@gmail.com>
2025-06-13 06:38:34 +0000

    objects

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -1,2 +1,23 @@
 // Singleton - when a constructor makes an object it will make only  of its own
 
+// if declared by literals, it wont be singleton
+// constructors alwasy give you singleton
+// object literals
+
+// following is the constructor method
+// Object.create
+// this will always yield singleton
+
+const JsUser = {
+    name : 'Mohsin',
+    age : 25,
+    location: 'Peshawar',
+    email: 'mohsin@gmail.com',
+    isLoggedIn: false,
+    lastLoginDays: ['Monday', 'Saturday']
+}
+
+// if you want to access an object, it has multiple ways
+
+console.log(JsUser.email);
+
